cb9da9fba6a3ec6ab1314ed384a7443dfdb51d71
Jan-Hendrik Willms <tleilax+github@gmail.com>
2012-04-04 19:54:10 +0200

    renamed 'clear cache' action to 'clear token'

--- app/controllers/client.php
+++ app/controllers/client.php
@@ -36,7 +36,7 @@ class ClientController extends StudipController
         }
 
         $clear_cache = sprintf('<a href="%s">%s</a>',
-                               $this->url_for('client/clear_cache'), _('Cache leeren'));
+                               $this->url_for('client/clear_cache'), _('Token löschen'));
         $this->setInfoboxImage('infobox/administration.png');
         $this->addToInfobox('Aktionen', $clear_cache, 'icons/16/black/refresh.png');
     }
@@ -48,7 +48,8 @@ class ClientController extends StudipController
         $cache = StudipCacheFactory::getCache();
         $cache->expire(self::ACCESS_TOKEN . $GLOBALS['user']->id);
         $cache->expire(self::REQUEST_TOKEN . $GLOBALS['user']->id);
-        PageLayout::postMessage(MessageBox::success(_('Der Cache wurde geleert.')));
+
+        PageLayout::postMessage(MessageBox::success(_('Das OAuth-Token wurde gelöscht.')));
         $this->redirect('client');
     }
 
